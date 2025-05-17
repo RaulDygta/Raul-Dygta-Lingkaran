8cb8e1a32c7bb3ba0c5421e6f6728503f1edb987
RaulDygta <rauldygta@gmail.com>
2025-05-18 02:02:59 +0700

    Update Lingkaran.java

--- src/lingkaran/Lingkaran.java
+++ src/lingkaran/Lingkaran.java
@@ -5,20 +5,18 @@
 package lingkaran;
 
 public class Lingkaran {
-    // Konstanta PI
     final double PI = 3.141592;
 
-    // Method untuk menghitung luas lingkaran
     void hitungLuas(double r) {
         double luas = PI * r * r;
 
-        // Menampilkan hasil dalam bentuk pecahan
+        // bilangan pecahan
         System.out.println("Luas Lingkaran (Bilangan Pecahan): " + luas);
 
-        // Menampilkan hasil dalam bentuk bilangan bulat (type-casting)
+        // bilangan bulat (type-casting)
         System.out.println("Luas Lingkaran (Bilangan Bulat): " + (int) luas);
 
-        // Menampilkan hasil dalam bentuk pembulatan (rounding)
+        // bilangan pembulatan (rounding)
         System.out.println("Luas Lingkaran (Pembulatan): " + Math.round(luas));
     }
 }
