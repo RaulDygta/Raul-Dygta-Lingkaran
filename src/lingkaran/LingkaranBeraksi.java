cfdaf8ffd9b84043dd2f49a0883e8f21d33f8648
RaulDygta <rauldygta@gmail.com>
2025-05-18 02:01:10 +0700

    Update LingkaranBeraksi.java

--- src/lingkaran/LingkaranBeraksi.java
+++ src/lingkaran/LingkaranBeraksi.java
@@ -6,10 +6,8 @@ package lingkaran;
 
 public class LingkaranBeraksi {
     public static void main(String[] args) {
-        // Membuat objek dari kelas Lingkaran
         Lingkaran lingkaran = new Lingkaran();
 
-        // Menghitung dan menampilkan luas lingkaran dengan jari-jari 11.78
         lingkaran.hitungLuas(11.78);
     }
 }
